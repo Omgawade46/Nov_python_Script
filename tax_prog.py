9cd8e752ddc6d2d9e2a95d8a155044e45524efd9
Omgawade46 <123226570+Omgawade46@users.noreply.github.com>
2024-12-11 08:54:54 +0530

    tax prog

---
+++ tax_prog.py
@@ -0,0 +1,5 @@
+# WAProg for taxiation. by the help of taking user input of the total money collectrd in the total year for that customer need to pay 18% of tax
+
+user=int(input("Enter the amount:"))
+total_tax=user*0.18
+print(total_tax)
